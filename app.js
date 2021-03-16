bda47dee9200740cb04c6cfe106e1aefdf3e8fbe
Alex Supkay <asupkay1124@gmail.com>
2021-03-15 21:04:43 -0400

    Render README on the main page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const utils = require('./utils');
+const md = require('markdown-it')();
 
 const app = express();
 const port = 3000;
@@ -19,13 +20,16 @@ app.engine(
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  const readme = md.render(utils.getReadme());
+  
   res.render('main', {
-    pieces: utils.getPieces()
+    pieces: utils.getPieces(),
+    readme
   });
 });
 
 app.get('/:route', (req, res) => {
-  res.render('main', {
+  res.render('piece', {
     scriptName: `${req.params.route}.js`,
     pieces: utils.getPieces()
   });
--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
 const getPieces = () => {
-  const files = fs.readdirSync('./public/js/pieces');
+  const files = fs.readdirSync(`${__dirname}/../public/js/pieces`);
   const pieces = files.map((file) => file.slice(0, -3))
   return pieces;
 }
 
+const getReadme = () => {
+  return fs.readFileSync(`${__dirname}/../README.md`, 'utf8');
+}
+
 module.exports = {
   getPieces,
+  getReadme,
 }
